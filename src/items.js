7a4b579d08569e9a109a4990c5dfbff1006d9f0f
Ed Kelly <ed.kelly@publicissapient.com>
2019-11-14 17:02:55 +0000

    Add images

--- src/items.js
+++ src/items.js
@@ -1,66 +1,50 @@
 export default [
   {
-    image: 'https://cataas.com/cat?width=400&height=400',
+    image: '/img/1.png',
     sound: 'https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3'
   },
   {
-    image: 'https://cataas.com/cat?width=400&height=400',
+    image: '/img/2.png',
     sound: 'https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3'
   },
   {
-    image: 'https://cataas.com/cat?width=400&height=400',
+    image: '/img/3.png',
     sound: 'https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3'
   },
   {
-    image: 'https://cataas.com/cat?width=400&height=400',
+    image: '/img/4.png',
     sound: 'https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3'
   },
   {
-    image: 'https://cataas.com/cat?width=400&height=400',
+    image: '/img/5.png',
     sound: 'https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3'
   },
   {
-    image: 'https://cataas.com/cat?width=400&height=400',
+    image: '/img/6.png',
     sound: 'https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3'
   },
   {
-    image: 'https://cataas.com/cat?width=400&height=400',
+    image: '/img/7.png',
     sound: 'https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3'
   },
   {
-    image: 'https://cataas.com/cat?width=400&height=400',
+    image: '/img/8.png',
     sound: 'https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3'
   },
   {
-    image: 'https://cataas.com/cat?width=400&height=400',
+    image: '/img/9.png',
     sound: 'https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3'
   },
   {
-    image: 'https://cataas.com/cat?width=400&height=400',
+    image: '/img/10.png',
     sound: 'https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3'
   },
   {
-    image: 'https://cataas.com/cat?width=400&height=400',
+    image: '/img/11.png',
     sound: 'https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3'
   },
   {
-    image: 'https://cataas.com/cat?width=400&height=400',
-    sound: 'https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3'
-  },
-  {
-    image: 'https://cataas.com/cat?width=400&height=400',
-    sound: 'https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3'
-  },
-  {
-    image: 'https://cataas.com/cat?width=400&height=400',
-    sound: 'https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3'
-  },
-  {
-    image: 'https://cataas.com/cat?width=400&height=400',
-    sound: 'https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3'
-  },
-  {
-    image: 'https://cataas.com/cat?width=400&height=400',
+    image: '/img/12.png',
     sound: 'https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3'
   }
 ];
